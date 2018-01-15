cc6fc8513bf06822e25f23e543cd8988cf39df5d
mike-carbone <mfcbone@gmail.com>
2018-01-15 18:34:09 -0500

    first push

---
+++ index.js
@@ -0,0 +1,8 @@
+function sayHello(){
+	console.log("hello from george");
+}
+
+function showCharlemagne(){
+	document.getElementById("shade").style.display = "block";
+	document.getElementById("aries").style.display = "block";
+}
